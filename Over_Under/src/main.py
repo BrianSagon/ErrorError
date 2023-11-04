9a2f4546bdb77d883846cb197cda80a9e2135595
BrianSagon <144291034+BrianSagon@users.noreply.github.com>
2023-11-04 06:56:56 -0500

    Update main.py

--- Over_Under/src/main.py
+++ Over_Under/src/main.py
@@ -7,8 +7,7 @@ brain=Brain()
 
 # Robot configuration code
 controller1 = Controller(PRIMARY)
-
-
+direction = "normal"
 
 # wait for rotation sensor to fully initialize
 wait(30, MSEC)
@@ -45,13 +44,16 @@ d["still"]["right"] = 0
 d["still"]["left"] = 0
 d["clockwise"] = -1
 d["counterclockwise"] = 1
+d["normal"] = 1
+d["reversed"] = -1
+
 class Motors:
         def __init__(self):
-            self.back_left=Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
-            self.front_left=Motor(Ports.PORT12, GearSetting.RATIO_18_1, False)
-            self.back_right=Motor(Ports.PORT20, GearSetting.RATIO_18_1, True)
-            self.front_right=Motor(Ports.PORT19, GearSetting.RATIO_18_1, True)
-            self.catapult=Motor(Ports.PORT4,GearSetting.RATIO_36_1, True)
+            self.back_left=Motor(Ports.PORT20, GearSetting.RATIO_18_1, False)
+            self.front_left=Motor(Ports.PORT19, GearSetting.RATIO_18_1, False)
+            self.back_right=Motor(Ports.PORT11, GearSetting.RATIO_18_1, True)
+            self.front_right=Motor(Ports.PORT12, GearSetting.RATIO_18_1, True)
+            self.puncher=Motor(Ports.PORT4,GearSetting.RATIO_36_1, False)
 
         def LeftMotorsVSpin(self, velocity):
             self.back_left.spin(FORWARD, velocity, PERCENT)
@@ -116,76 +118,89 @@ class Motors:
             self.back_right.spin_to_position(degree_amount  * d[clockdirection], DEGREES, speed, PERCENT, True)
         
         def Drive(self):
+            brain.screen.print("DriveThread")
             while True:
-                drive_train.LeftMotorsVSpin(controller1.axis3.position()+(controller1.axis1.position()/(1+abs(controller1.axis3.position()/100))))
-                drive_train.RightMotorsVSpin(controller1.axis3.position()-(controller1.axis1.position()/(1+abs(controller1.axis3.position()/100))))
-                sleep(5)
+                drive_train.LeftMotorsVSpin((controller1.axis3.position()+(d[direction]*(controller1.axis1.position())/(1+abs(controller1.axis3.position()/100))))*d[direction])
+                drive_train.RightMotorsVSpin((controller1.axis3.position()-(d[direction]*(controller1.axis1.position())/(1+abs(controller1.axis3.position()/100))))*d[direction])
+                wait(5, MSEC)
 
-        def PositionCatapult(self, velocity):
-            brain.screen.print("Line 125")
-            self.catapult.set_max_torque(4, PERCENT)
-            self.catapult.spin(FORWARD, velocity, PERCENT)             #Change this to run until the motor experiences a ton of torque
-            wait(.4, SECONDS)
-            self.catapult.stop(COAST)
+        def ChangeDriveDirection(self):
+            global direction
+            if direction == "normal":
+                direction = "reversed"
+            else:
+                direction = "normal"
+
+        def PositionPuncher(self):
+            brain.screen.print("Line 127")
+            self.puncher.set_max_torque(2, PERCENT)
+            self.puncher.spin(FORWARD, 100, PERCENT)             #Change this to run until the motor experiences a ton of torque
+            wait(.3, SECONDS)
+            self.puncher.stop(COAST)
             wait(.2, SECONDS)
-            self.catapult.set_max_torque(100, PERCENT)
+            self.puncher.set_max_torque(100, PERCENT)
 
-        def PrimeCatapult(self):
+        def PrimePuncher(self):
             brain.screen.print("line 134")
-            self.catapult.set_position(0, DEGREES)
-            self.catapult.spin_to_position(180, DEGREES, 100, PERCENT, True)
-            controller1.buttonB.pressed(self.LaunchCatapult)
+            self.puncher.set_position(0, DEGREES)
+            self.puncher.spin_to_position(250, DEGREES, 100, PERCENT, True)
 
-        def LaunchCatapult(self):
-            self.catapult.set_position(0, DEGREES)
-            self.catapult.spin_to_position(60, DEGREES, 100, PERCENT)
+        def LaunchPuncher(self):
+            self.puncher.set_position(0, DEGREES)
+            self.puncher.spin_to_position(-60, DEGREES, 100, PERCENT)
         
         def UnprimeCatapult(self):
-            self.catapult.set_position(0, DEGREES)
-            self.catapult.spin_to_position(-160, DEGREES, 100, PERCENT)
-            self.PositionCatapult
+            self.puncher.set_position(0, DEGREES)
+            self.puncher.spin_to_position(-250, DEGREES, 100, PERCENT)
         
-        def CatapultNotPrimed(self):
-            wait(.2, SECONDS)
+        def PuncherNotPrimed(self):
+            brain.screen.print("line150")
             while True:
                 if controller1.buttonA.pressing() == True:
                     brain.screen.print("Line 152")
-                    self.PrimeCatapult()
-                    #wait(.2, SECONDS)
-                    Thread(self.CatapultIsPrimed)
-                    break
-                sleep(10)
+                    self.PrimePuncher()
+                    self.PrimedThread = Thread(self.PuncherIsPrimed())
+                    game.NotPrimedThread.stop()
+                wait(5, MSEC)
             
-        def CatapultIsPrimed(self):
-            wait(.2, SECONDS)
+        def PuncherIsPrimed(self):
             brain.screen.clear_screen()
             brain.screen.print("CatapultIsPrimed Thread IS On")
             while True:
                 if controller1.buttonA.pressing() == True:
                     self.UnprimeCatapult()
-                    Thread(self.CatapultNotPrimed)
-                    break
+                    self.NotPrimedThread = Thread(drive_train.PuncherNotPrimed())
+                    self.PrimedThread.stop()
                 if controller1.buttonB.pressing() == True:
-                    self.LaunchCatapult()
-                    self.PositionCatapult(100)
-                    self.PrimeCatapult()
-                sleep(10)
+                    self.LaunchPuncher()
+                    self.PrimePuncher()
+                wait(5, MSEC)
 
-                
-        
-        
+        def Puncher(self):
+            while True:
+                if controller1.buttonA.pressing() == True:
+                    self.puncher.set_position(0, DEGREES)
+                    self.puncher.spin_to_position(360, DEGREES, 100, PERCENT, True)
+
+        def SpinPuncher(self):
+            self.puncher.set_position(0, DEGREES)
+            self.puncher.spin_to_position(360, DEGREES, 100, PERCENT, True)
             
-                
-                
+
+class Game():
+        def driverControl(self):
+            brain.screen.print("line 169")
+            self.NotPrimedThread = Thread(drive_train.Puncher)
+            self.DrivingThread = Thread(drive_train.Drive)
+            controller1.buttonL2.pressed(drive_train.ChangeDriveDirection)
+        def autonomous(self):
+            drive_train.puncher.spin_to_position(360, DEGREES, 100, PERCENT)
 
 drive_train = Motors()
+game = Game()
 
 
 
 #Main
-def 
-drive_train.PositionCatapult(100)
-Thread(drive_train.Drive)
-Thread(drive_train.CatapultNotPrimed)
 
-    
+competition = Competition(game.driverControl, game.autonomous)
