88c68750cb537d35270c76d0083989dacf4dc562
BrianSagon <144291034+BrianSagon@users.noreply.github.com>
2024-01-24 21:34:02 -0600

    1/24 Update for comp

--- Over_Under/src/main.py
+++ Over_Under/src/main.py
@@ -1,6 +1,6 @@
 #region VEXcode Generated Robot Configuration
 from vex import *
-
+import time
 
 # Brain should be defined by default
 brain=Brain()
@@ -10,9 +10,9 @@ controller1 = Controller(PRIMARY)
 direction = "normal"
 wingsenabled = False
 MotorsStuck = False
-kP = .32
+kP = .35
 kI = 0
-kD = 3
+kD = 2.5
 # wait for rotation sensor to fully initialize
 wait(30, MSEC)
 #endregion VEXcode Generated Robot Configuration
@@ -44,8 +44,8 @@ d["right"] = {}
 d["right"]["right"] = -1
 d["right"]["left"] = 1
 d["left"] = {}
-d["left"]["right"] = -1
-d["left"]["left"] = 1
+d["left"]["right"] = 1
+d["left"]["left"] = -1
 d["still"] = {}
 d["still"]["right"] = 0
 d["still"]["left"] = 0
@@ -85,95 +85,20 @@ class Motors:
             self.back_right.set_position(0, DEGREES)
             self.front_left.set_position(0, DEGREES)
             self.back_left.set_position(0, DEGREES)
-            self.StillSpinningTestThread = Thread(self.StillSpinningTest, (degree_amount,))
             self.back_left.spin_to_position(degree_amount * d[direction]["left"], DEGREES, speed, PERCENT, False)
             self.front_left.spin_to_position(degree_amount * d[direction]["left"], DEGREES, speed, PERCENT, False)
             self.front_right.spin_to_position(degree_amount * d[direction]["right"], DEGREES, speed, PERCENT, False)
             self.back_right.spin_to_position(degree_amount  * d[direction]["right"], DEGREES, speed, PERCENT, True)    #True since it is the last one to be called, activating it and the other spin_to's next that have been activated
 
         def StillSpinningTest(self, target):
-            global MotorsStuck
             wait(.2, MSEC)
-            self.back_leftDerivTestThread = Thread(self.back_leftDerivTest, (target,))
-            self.back_rightDerivTestThread = Thread(self.back_rightDerivTest, (target,))
-            self.front_leftDerivTestThread = Thread(self.front_leftDerivTest, (target,))
-            self.front_rightDerivTestThread = Thread(self.front_rightDerivTest, (target,))
-            while True:
-                if MotorsStuck == True:
+            """while True:
+                if self.front_left.is_spinning() == False or self.front_right.is_spinning() == False or self.back_left.is_spinning() == False or self.back_right.is_spinning() == False:
                     self.back_left.set_position(target, DEGREES)
                     self.back_right.set_position(target, DEGREES)
                     self.front_left.set_position(target, DEGREES)
                     self.front_right.set_position(target, DEGREES)
-                    self.back_leftDerivTestThread.stop()
-                    self.back_rightDerivTestThread.stop()
-                    self.front_leftDerivTestThread.stop()
-                    self.front_rightDerivTestThread.stop()
-                    self.BackLeftPidThread.stop()
-                    self.BackRightPidThread.stop()
-                    self.FrontRightPidThread.stop()
-                    self.FrontLeftPidThread.stop()
-                    MotorsStuck = False
-                    break
-
-        def back_rightDerivTest(self, target):
-            global MotorsStuck
-            preverror = target
-            x = 0
-            while True:
-                error = target - self.back_right.position()
-                dP = preverror - error
-                preverror = error
-                if dP > 1:
-                    x = x+1
-                    if x > 10:
-                        break
-                wait(20, MSEC)
-            MotorsStuck = True
-
-        def back_leftDerivTest(self, target):
-            global MotorsStuck
-            preverror = target
-            x = 0
-            while True:
-                error = target - self.back_left.position()
-                dP = preverror - error
-                preverror = error
-                if dP > 1:
-                    x = x+1
-                    if x > 10:
-                        break
-                wait(20, MSEC)
-            MotorsStuck = True
-
-        def front_rightDerivTest(self, target):
-            global MotorsStuck
-            preverror = target
-            x = 0
-            while True:
-                error = target - self.front_right.position()
-                dP = preverror - error
-                preverror = error
-                if dP > 1:
-                    x = x+1
-                    if x > 10:
-                        break
-                wait(20, MSEC)
-            MotorsStuck = True
-
-        def front_leftDerivTest(self, target):
-            global MotorsStuck
-            preverror = target
-            x = 0
-            while True:
-                error = target - self.front_left.position()
-                dP = preverror - error
-                preverror = error
-                if dP > 1:
-                    x = x+1
-                    if x > 10:
-                        break
-                wait(20, MSEC)
-            MotorsStuck = True
+                    break"""
 
         def MotorsStop(self):
             self.front_left.stop()
@@ -194,8 +119,15 @@ class Motors:
             global kD
             prevError = 0
             integral = 0
+            i = 0
+            derivative = 0
+            initial_seconds = time.time()
             while True:
+                delta_seconds = time.time()-initial_seconds
                 error = target-self.front_left.position()
+                if (delta_seconds > 1 and derivative == 0) or delta_seconds > 4:
+                    drive_train.MotorsBrake()
+                    break
                 integral = integral + error
                 if error <= target:
                     integral = 0
@@ -203,8 +135,13 @@ class Motors:
                     integral = 0
                 derivative = error - prevError
                 prevError = error
+                if i < 80:
+                    i += 1
+                    brain.screen.clear_screen()
+                    brain.screen.set_cursor(1,1)
+                    brain.screen.print(i)
                 power = error * kP + integral * kI + derivative * kD
-                self.front_left.spin(FORWARD, power, PERCENT)
+                self.front_left.spin(FORWARD, power*(i/80), PERCENT)
                 wait(20, MSEC)
             
         def FrontRightPid(self, target):
@@ -214,8 +151,15 @@ class Motors:
             global kD
             prevError = 0
             integral = 0
+            i = 0
+            derivative = 0
+            initial_seconds = time.time()
             while True:
+                delta_seconds = time.time()-initial_seconds
                 error = target-self.front_right.position()
+                if (delta_seconds > 1 and derivative == 0) or delta_seconds > 4:
+                    drive_train.MotorsBrake()
+                    break
                 integral = integral + error
                 if error <= target:
                     integral = 0
@@ -224,7 +168,9 @@ class Motors:
                 derivative = error - prevError
                 prevError = error
                 power = error * kP + integral * kI + derivative * kD
-                self.front_right.spin(FORWARD, power, PERCENT)
+                if i < 80:
+                    i += 1
+                self.front_right.spin(FORWARD, power*(i/80), PERCENT)
                 wait(20, MSEC)
 
         def BackLeftPid(self, target):
@@ -234,8 +180,15 @@ class Motors:
             global kD
             prevError = 0
             integral = 0
+            i = 0
+            derivative = 0
+            initial_seconds = time.time()
             while True:
+                delta_seconds = time.time()-initial_seconds
                 error = target-self.back_left.position()
+                if (delta_seconds > 1 and derivative == 0) or delta_seconds > 4:
+                    drive_train.MotorsBrake()
+                    break
                 integral = integral + error
                 if error <= target:
                     integral = 0
@@ -244,7 +197,9 @@ class Motors:
                 derivative = error - prevError
                 prevError = error
                 power = error * kP + integral * kI + derivative * kD
-                self.back_left.spin(FORWARD, power, PERCENT)
+                if i < 80:
+                    i += 1
+                self.back_left.spin(FORWARD, power*(i/80), PERCENT)
                 wait(20, MSEC)
 
         def BackRightPid(self, target):
@@ -254,8 +209,15 @@ class Motors:
             global kD
             prevError = 0
             integral = 0
+            i = 0
+            derivative = 0
+            initial_seconds = time.time()
             while True:
+                delta_seconds = time.time()-initial_seconds
                 error = target-self.back_right.position()
+                if (delta_seconds > 1 and derivative == 0) or delta_seconds > 4:
+                    drive_train.MotorsBrake()
+                    break
                 integral = integral + error
                 if error <= target:
                     integral = 0
@@ -264,7 +226,9 @@ class Motors:
                 derivative = error - prevError
                 prevError = error
                 power = error * kP + integral * kI + derivative * kD
-                self.back_right.spin(FORWARD, power, PERCENT)
+                if i < 80:
+                    i += 1
+                self.back_right.spin(FORWARD, power*(i/80), PERCENT)
                 wait(20, MSEC)
 
         def MoveTo(self, forward, right, direction):
@@ -273,7 +237,7 @@ class Motors:
                 if forward > 0:
                     MoveToDirection = "forward"
                 elif forward < 0:
-                    MoveToDirection = "back"
+                    MoveToDirection = "right"
             elif right < 0:
                 MoveToDirection = "left"
             elif right > 0:
@@ -284,27 +248,36 @@ class Motors:
             radians = 0
             if forward == 0:                 #I'm not adding a check for if both are zero, as that should never happen
                 if right < 0:                  #this is all the calculation for the amount of radians that the robot needs to turn
-                    radians = 1.57079 * d[direction]
+                    radians = 1.57079 * d["direction"][direction]
                 if right > 0:
-                    radians = -1.57079 * d[direction]
+                    radians = 1.57079 * d["direction"][direction]
+            elif right == 0:
+                if forward < 0:
+                    radians = 3.14159 * d["direction"][direction]
+                if forward > 0:
+                    radians = 3.14159 * d["direction"][direction]
             else:
                 radians = (math.atan(right/forward))
                 if forward < 0 and right < 0:
-                    radians -= math.pi * d[direction]
+                    radians -= math.pi * d["direction"][direction]
                 if forward < 0 and right > 0:
-                    radians += math.pi * d[direction]
+                    radians += math.pi * d["direction"][direction]
             lengthOfArc = 4.5518 * radians          #10 inches is the diameter between wheels of the robot, 5 inches is the radius   #Changed to 4.5518 as it seems to be more accurate, possibly because the back motors are pushing it forward as well.
-            Turns = lengthOfArc/11.52        #10.99 is the circumference of the wheels
+            Turns = lengthOfArc/10.9        #10.99 is the circumference of the wheels
             degree = Turns * 360
-            self.MotorsDSpin(degree, 20, MoveToDirection)
+            if degree != 0:
+                self.BackRightPidThread = Thread(self.BackRightPid, (degree * d["right"][MoveToDirection],))
+                self.FrontLeftPidThread = Thread(self.FrontLeftPid, (degree * d["left"][MoveToDirection],))
+                self.FrontRightPidThread = Thread(self.FrontRightPid, (degree * d["right"][MoveToDirection],))
+                self.BackLeftPid(degree * d["left"][MoveToDirection])
 
-            Turns = (length*12)/10.99          #length is multiplied by 12 so that it is defined in inches rather than feet; the 12.566 is also in inches so multiplying it by 12 cancels them into just turns of the wheel
+            Turns = (length*12)/12.2          #length is multiplied by 12 so that it is defined in inches rather than feet; the 12.566 is also in inches so multiplying it by 12 cancels them into just turns of the wheel
             degree = Turns * 360
             self.BackRightPidThread = Thread(self.BackRightPid, (degree * d["direction"][direction],))
             self.FrontLeftPidThread = Thread(self.FrontLeftPid, (degree * d["direction"][direction],))
             self.FrontRightPidThread = Thread(self.FrontRightPid, (degree * d["direction"][direction],))
-            self.BackLeftPidThread = Thread(self.BackLeftPid, (degree * d["direction"][direction],))
-            self.StillSpinningTest(degree)
+            self.BackLeftPid(degree * d["direction"][direction])
+            #self.StillSpinningThread = Thread(self.StillSpinningTest, (degree,))
 
             #self.MotorsDSpin(degree * d["direction"][direction], 100, "forward")      #The direction here should always be forward since it is facing its target
 
@@ -382,7 +355,7 @@ class Motors:
         def Puncher(self):
             while True:
                 if controller1.buttonA.pressing() == True:
-                    self.SpinPuncher
+                    self.SpinPuncher()
 
         def SpinPuncher(self):
             self.puncher_group.set_position(0, DEGREES)
@@ -411,7 +384,10 @@ class Motors:
             
             
 class Game():
-        def driverControl(self):
+        def user_control(self):
+            brain.screen.clear_screen()
+            brain.screen.print("user control code")
+            #User control code
             drive_train.DisableWings()
             self.PuncherThread = Thread(drive_train.Puncher)
             self.DrivingThread = Thread(drive_train.Drive)
@@ -419,11 +395,19 @@ class Game():
             controller1.buttonL2.pressed(drive_train.ChangeDriveDirection)
             
         def autonomous(self):
+            brain.screen.clear_screen()
+            brain.screen.print("auton code")
+            #auton code
             drive_train.DisableWings()
-            drive_train.SpinPuncher()
-            drive_train.MotorsBrake()
+            #drive_train.MoveTo(1, 0, "forward")
+            drive_train.MoveTo(-1, 0, "forward")
             #drive_train.Turn("clockwise", 30, 100)
             #drive_train.MoveTo(3.92, 0, "normal")
+
+        def pre_autonomous(self):
+            brain.screen.clear_screen()
+            brain.screen.print("pre auton code")
+            #pre auton code
 
 
 
@@ -433,5 +417,5 @@ game = Game()
 
 
 #Main
-competition = Competition(game.driverControl, game.autonomous)
-game.driverControl()
+comp = Competition(game.user_control, game.autonomous)
+game.pre_autonomous()
